d6f625390dff4fa73733a189c58b88b865db0f4c
wei.hou <wei.hou@mihoyo.com>
2022-09-02 00:29:27 +0800

    deploy

--- assets/document_articles_index.md.9bcb9a82.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./app.d243facf.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"document/articles/index.md"}'),r={name:"document/articles/index.md"};function n(c,o,s,d,i,p){return a(),t("div")}var l=e(r,[["render",n]]);export{m as __pageData,l as default};
